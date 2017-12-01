a4d34d3c8b240886685aec4ccaa07c92cf985380
Shin Yoon <shiyoon@stubhub.com>
2017-12-01 12:25:16 -0800

    Refactored layout to be less jittery

--- src/MainView/Bot/Bot.js
+++ src/MainView/Bot/Bot.js
@@ -11,17 +11,43 @@ export default function Bot(props) {
     const styles = StyleSheet.create({
         wrapper: {
             display: 'grid',
+            gridTemplateColumns: '100%',
+            gridAutoRows: '1fr',
+            minWidth: '30em',
+            minHeight: '30em'
+        },
+        speechWrapper: {
+            display: 'grid',
+            alignItems: 'end',
+            justifyItems: 'center',
+        },
+        profileWrapper: {
+            display: 'grid',
             alignItems: 'center',
-            justifyItems: 'center'
+            justifyItems: 'center',
+        },
+        bubbleWrapper: {
+            display: 'grid',
+            alignItems: 'center',
+            justifyItems: 'center',
         }
     })
     
     
     return (
         <div className={css(styles.wrapper)}>
-            <SpeechBubble response={props.message} />
-            <ProfileBubble />
-            <LinkBubbles shown={props.showLinks}/>
+            <div className={css(styles.speechWrapper)}>
+                <SpeechBubble response={props.message} />
+                
+            </div>
+            <div className={css(styles.profileWrapper)}>
+                <ProfileBubble />
+            
+            </div>
+            <div className={css(styles.bubbleWrapper)}>
+                <LinkBubbles shown={props.showLinks}/>
+            
+            </div>
         </div>
     )
 }
