cd2604cf1f7d21c05d2874e4166d9cfdd06c0039
Shin Yoon <shiyoon@stubhub.com>
2018-01-21 15:34:18 -0800

    Removed service worker code

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import register from './registerServiceWorker';
+// import register from './registerServiceWorker';
 
 import MainView from './MainView/MainView';
 
 
 ReactDOM.render(<MainView />, document.getElementById('root'));
-register();
+// register();
