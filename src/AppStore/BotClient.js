aa743d19fda029629dc4e32b71f4e06a0d486434
Shin Yoon <shiyoon@stubhub.com>
2018-01-21 12:52:45 -0800

    flush

--- src/AppStore/BotClient.js
+++ src/AppStore/BotClient.js
@@ -2,7 +2,7 @@ import {ApiAiClient} from 'api-ai-javascript';
 
 
 // TODO: sanitized this
-const ACCESS_TOKEN = '450cf73c3fb84f028648fa9b4521b252';
+const ACCESS_TOKEN = 'f2fce62f9d5c47159bd1c5eed9eb5355';
 
 const client = new ApiAiClient({accessToken: ACCESS_TOKEN});
 
