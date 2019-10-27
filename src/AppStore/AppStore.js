4c3212bcf48ae7a9bd2caf1541e9360be76ba466
Shin Yoon <shiyoon@stubhub.com>
2019-10-27 12:15:35 -0700

    refactor

--- src/AppStore/AppStore.js
+++ src/AppStore/AppStore.js
@@ -1,5 +1,5 @@
 import { extendObservable, action } from 'mobx';
-import client from './BotClient';
+import dialogflowClient from './BotClient';
 
 
 const defaultMessage = 'Welcome to my website! Please check out the links below, or ask me something!';
@@ -16,26 +16,31 @@ class AppStore {
                 console.log('Guest said : ' + msg);
                 this.currentMessage = loadingMessage;
                 this.customRes = null;
-                client.textRequest(msg)
-                    .then((res) => {
-                        let customRes = res.result.fulfillment.messages.find((e) => {
-                            return e.type === 4;
-                        });
-
-                        if (customRes) {
-                            this.customRes = customRes.payload;
-                            this.currentMessage = loadingMessage;
-                        } else {
-                            this.currentMessage = res.result.fulfillment.speech || 'Come again?';
-                        }
-                    })
-                    .catch((err) => {
-                        console.error(err);
-                        this.currentMessage = 'Oops something went wrong...I seem to be experiencing technical issues :(';
-
-                    });
+                this.sendGuestMessageToBot(msg)
             })
         });
+    }
+
+    sendGuestMessageToBot(msg) {
+        dialogflowClient.textRequest(msg)
+            .then((res) => {
+                let customRes = res.result.fulfillment.messages.find((e) => {
+                    return e.type === 4;
+                });
+
+                if (customRes) {
+                    this.customRes = customRes.payload;
+                    this.currentMessage = loadingMessage;
+                } else {
+                    this.currentMessage = res.result.fulfillment.speech || 'Come again?';
+                }
+            })
+            .catch((err) => {
+                console.error(err);
+                this.currentMessage = 'Oops something went wrong...I seem to be experiencing technical issues :(';
+
+            });
+
     }
 
 }
