ab0be1389de6dbd532510627e280d36e15762ca4
Shin Yoon <shinmyung0@gmail.com>
2019-11-03 06:54:46 -0800

    cleanup

--- api/dialogflowClient.js
+++ api/dialogflowClient.js
@@ -1,12 +1,7 @@
 const dialogflow = require("dialogflow");
 
 const sessionClient = new dialogflow.SessionsClient();
-// A unique identifier for the given session
 
-/**
- * Send a query to the dialogflow agent, and return the query result.
- * @param {string} projectId The project to be used
- */
 async function sayToBot(message, sessionId) {
   const projectId = await sessionClient.getProjectId();
   const sessionPath = sessionClient.sessionPath(projectId, sessionId);
@@ -16,15 +11,12 @@ async function sayToBot(message, sessionId) {
     session: sessionPath,
     queryInput: {
       text: {
-        // The query to send to the dialogflow agent
         text: message,
-        // The language used by the client (en-US)
         languageCode: "en-US"
       }
     }
   };
 
-  // Send request and log result
   const responses = await sessionClient.detectIntent(request);
   const result = responses[0].queryResult;
 
